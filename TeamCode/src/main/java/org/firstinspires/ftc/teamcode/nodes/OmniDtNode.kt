a1a9b60cff63e5d9779ce400fa62b78f08939987
Jacob <Enemiesgate@gmail.com>
2017-11-15 13:26:09 -0500

    Added slow mode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/OmniDtNode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/OmniDtNode.kt
@@ -41,6 +41,8 @@ class OmniDtNode : Node(){
         val findhighestinthis = sums.map{abs(it)}
         val highestsum:Float? = findhighestinthis.max()
         var attenuationfactor = 1f
+        val fast = false
+        val slowrate = .6f
         if (highestsum == null){
             attenuationfactor = 1f
         }
@@ -51,8 +53,13 @@ class OmniDtNode : Node(){
             attenuationfactor = 1f
         }
 
-        val finalvals = sums.map { it / attenuationfactor }
-        return finalvals
+       if (fast) {
+           val finalvals = sums.map { it / attenuationfactor }
+           return finalvals
+       } else {
+           val finalvals = sums.map { it / attenuationfactor * slowrate }
+           return finalvals
+       }
     }
 
 }
