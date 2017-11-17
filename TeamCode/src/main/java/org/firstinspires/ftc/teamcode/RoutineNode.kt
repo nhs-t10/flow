94c77574bdc71f27be2af7530845f58f8376b4e2
afk26 <akiely26@gmail.com>
2017-11-17 14:54:19 -0500

    new node

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoutineNode.kt
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode
+
+/**
+ * Created by dvw06 on 11/16/17.
+ */
+abstract class RoutineNode : Node() {
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/messages/GlyphMechanisms.kt
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.messages
+
+/**
+ * Created by dvw06 on 11/14/17.
+ */
+
+//data class ToggleLowe : Message
