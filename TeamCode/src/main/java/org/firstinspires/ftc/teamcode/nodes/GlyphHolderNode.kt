b3514b8793d8e3f12483a17dff313eb8fd99542d
shaashwat <shaash317@gmail.com>
2017-12-03 13:58:22 -0500

    commented and cleaned up GlyphHolderNode

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/GlyphHolderNode.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/GlyphHolderNode.kt
@@ -2,36 +2,28 @@ package org.firstinspires.ftc.teamcode.nodes
 
 import org.firstinspires.ftc.teamcode.Node
 import org.firstinspires.ftc.teamcode.messages.*
-import org.firstinspires.ftc.teamcode.util.whenDown
 
 /**
  * Created by shaash on 10/26/17.
  */
 
 class GlyphHolderNode : Node("Glyph Holder") {
-
-    val bottomFullOpenPosition = 0.5
+    // Bottom Holder Servo Position Constants
     val bottomClosedPosition = 1.0
     val bottomOpenPosition = 0.65
-    val bottomHalfPosition = 0.8
-
+    val bottomDropPosition = 0.8
+    // Top Holder Servo Position Constants
     val topClosedPosition = 0.7
     val topOpenPosition = 0.05
-    val topHalfPosition = 0.5
-
-    val holderLOpenPosition = 1.0
-    val holderLClosedPosition = 0.0
-    val holderROpenPosition = 1.0
-    val holderRClosedPosition = 0.0
+    val topDropPosition = 0.5
+    // Holder States
     var bottomIsOpen = true
     var topIsOpen = true
-    var holderIsOpen= true
 
     override fun subscriptions() {
         this.subscribe("/glyph/upper", {upper(it)})
         this.subscribe("/glyph/lower", {lower(it)})
-        this.subscribe("/gamepad1/x", {releasePosition(it)})
-        //this.subscribe("/gamepad1/x", whenDown { holder() })
+        this.subscribe("/gamepad1/x", {dropPosition(it)}) // Opens holders slightly so the glyphs can drop.
     }
 
     fun findState (current : Boolean, state : GripperState) = when (state) {
@@ -50,25 +42,10 @@ class GlyphHolderNode : Node("Glyph Holder") {
         topIsOpen = findState(topIsOpen, state)
         this.publish("/servos/topServo", ServoMsg(if (topIsOpen) topOpenPosition else topClosedPosition, priority = 1))
     }
-    fun releasePosition(m : Message) {
-        val isPressed = m as GamepadButtonMsg
+    fun dropPosition(m : Message) {
         bottomIsOpen = false
         topIsOpen = false
-        this.publish("/servos/bottomServo", ServoMsg(position = bottomHalfPosition, priority = 1))
-        this.publish("/servos/topServo", ServoMsg(position = topHalfPosition, priority = 1))
-
+        this.publish("/servos/bottomServo", ServoMsg(position = bottomDropPosition, priority = 1))
+        this.publish("/servos/topServo", ServoMsg(position = topDropPosition, priority = 1))
     }
-    /*
-    fun holder(){
-        if(holderIsOpen){
-            this.publish("/servos/holderServoL", ServoMsg(holderLClosedPosition, priority = 1))
-            this.publish("/servos/holderServoR", ServoMsg(holderRClosedPosition, priority = 1))
-            holderIsOpen = false
-        } else {
-            this.publish("/servos/holderServoL", ServoMsg(holderLOpenPosition, priority = 1))
-            this.publish("/servos/holderServoR", ServoMsg(holderROpenPosition, priority = 1))
-            holderIsOpen = true
-        }
-    }
-    */
 }
