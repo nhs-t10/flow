78a61c31473ca55fc942673da8e2d2b5e0aa004e
shaashwat <shaash317@gmail.com>
2017-12-06 22:09:33 -0500

    minor fixes to KnockerRoutine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/routines/KnockerRoutine.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/routines/KnockerRoutine.kt
@@ -23,19 +23,20 @@ class KnockerRoutine(val team : TeamColor = TeamColor.RED) : RoutineNode (name =
     fun doOnce(m : Message){
         if(!turned){
             knockBall(m)
-            turned = true
         }
     }
 
     fun knockBall(m : Message){
         val (red, blue, green) = m as ColorMsg
         if (red > (blue+50)){
+            turned = true
             if(team == TeamColor.RED){
                 this.publish("/AngleTurning/turnTo", AngleTurnMsg(30.0, {retractKnocker()}, 1))
             } else {
                 this.publish("/AngleTurning/turnTo", AngleTurnMsg(-30.0, {retractKnocker()}, 1))
             }
         } else if (blue > (red+50)) {
+            turned = true
             if (team == TeamColor.BLUE) {
                 this.publish("/AngleTurning/turnTo", AngleTurnMsg(-30.0, { retractKnocker() }, 1))
             } else {
@@ -48,5 +49,6 @@ class KnockerRoutine(val team : TeamColor = TeamColor.RED) : RoutineNode (name =
 
     fun retractKnocker(){
         this.publish("/servos/knocker", ServoMsg(upPosition, 1))
+        end()
     }
 }
