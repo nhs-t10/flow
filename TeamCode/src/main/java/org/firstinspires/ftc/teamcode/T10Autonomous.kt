111fd76c9584067ff3bea0403cf32638d363b38d
Max Krieger <themaxaxis@gmail.com>
2017-12-06 21:11:35 -0500

    shaash:b:at didn't remove spinroutine

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T10Autonomous.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T10Autonomous.kt
@@ -1,10 +1,8 @@
 package org.firstinspires.ftc.teamcode
 
-import android.widget.Spinner
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.OpMode
 import org.firstinspires.ftc.teamcode.nodes.*
-import org.firstinspires.ftc.teamcode.nodes.routines.SpinRoutine
 
 /**
  * Created by shaash on 10/26/17.
@@ -18,7 +16,6 @@ class T10Autonomous : OpMode(){
 
     override fun init() {
         routine = RoutineGroup(listOf(
-                SpinRoutine()
 
         ))
         nodes = listOf(
