86ec212d87ebc490c97839b116f959cca4e50e0b
Max Krieger <git+themaxaxis@gmail.com>
2018-01-24 15:04:50 -0500

    rolling avg

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/routines/DriveToCryptoboxRoutine.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/routines/DriveToCryptoboxRoutine.kt
@@ -10,12 +10,22 @@ import org.firstinspires.ftc.teamcode.messages.OmniDrive
  */
 
 class DriveToCryptoboxRoutine : RoutineNode("Drive to cryptobox") {
+    var buffer = Pair(0.0, 0.0)
+
     override fun subscriptions() {
-        subscribe("/analog/ultra1", {onDistance(it)})
+        subscribe("/analog/ultra1", {bufferDistance(it)})
     }
 
-    fun onDistance(m: Message) {
+    fun bufferDistance(m: Message) {
         val (value) = m as AnalogMsg
+        // Making sure a drastic change is legit
+        if (Math.abs(value - (buffer.first + buffer.second)/2) <= 0.03) {
+            onDistance(value)
+        }
+        buffer = Pair(buffer.second, value)
+    }
+
+    fun onDistance(value : Double) {
         if (value <= 0.05) {
             publish("/drive", OmniDrive(0f, 0f, 0f, 1))
             end()
