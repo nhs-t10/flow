dd893b4ea68693dfac6af1c9cbb239ca42194c27
afk26 <akiely26@gmail.com>
2018-02-08 16:22:30 -0500

    lit fam squad sorry for hitting that :b:eet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/routines/StopAtCryptoboxRoutine.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/nodes/routines/StopAtCryptoboxRoutine.kt
@@ -11,28 +11,44 @@ import org.firstinspires.ftc.teamcode.util.TeamColor
  */
 
 class StopAtCryptoboxRoutine(val vumark: RelicRecoveryVuMark) : RoutineNode("Drive to Cryptobox") {
+    val distancethresh = 0.5
+    var isCloseEnough = false
     override fun start() {
         this.publish("/drive", OmniDrive(upDown = 0.3f, rotation = 0f, leftRight = 0f, priority = 1))
 
     }
     override fun subscriptions() {
+        subscribe("/analog/ultra1", {setTrue(it)})
         subscribe("/digital/touch1", {
             if(vumark==RelicRecoveryVuMark.LEFT){stopIfThere(it, 3)}
             else if(vumark==RelicRecoveryVuMark.CENTER){stopIfThere(it, 2)}
             else if(vumark==RelicRecoveryVuMark.RIGHT){stopIfThere(it, 1)}
-
         })
     }
-    fun stopIfThere(m: Message, flanges: Int){
+    fun setTrue(m : Message){
+        if(!isCloseEnough){
+            val (value) = m as AnalogMsg
+            if(value < distancethresh){
+                this.publish("/drive", OmniDrive(upDown = 0f, rotation = 0f, leftRight = -0.2f, priority = 1))
+            } else {
+                this.publish("/drive", OmniDrive(upDown = 0.2f, rotation = 0f, leftRight = 0f, priority = 1))
+                isCloseEnough = true
+            }
+        }
+    }
 
-        val (value) = m as DigitalMsg
-        var counter = 0
-        if((counter<flanges) && value) {
-            counter++
-            publish("/status", TextMsg("flanges detected: $counter"))
-        } else {
-            this.publish("/drive", OmniDrive(upDown = 0.0f, rotation = 0f, leftRight = 0f, priority = 1))
-            end()
+    fun stopIfThere(m: Message, flanges: Int){
+        if(isCloseEnough){
+            val (value) = m as DigitalMsg
+            var counter = 0
+
+            if((counter<flanges) && value) {
+                counter++
+                publish("/status", TextMsg("flanges detected: $counter"))
+            } else {
+                this.publish("/drive", OmniDrive(upDown = 0.0f, rotation = 0f, leftRight = 0f, priority = 1))
+                end()
+            }
         }
     }
 }
